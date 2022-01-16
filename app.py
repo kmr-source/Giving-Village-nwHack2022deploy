515534739bdce6e44d8445bb7a9c860534b8c614
Jessica Hong <jeessicahong@gmail.com>
2022-01-16 04:25:08 -0800

    moved data over to app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import json
-import requests
-import test_data
 import os
 
 from flask import Flask, render_template
@@ -9,13 +6,13 @@ from flask_sqlalchemy import SQLAlchemy
 from uuid import uuid4
 
 # connecting host to heroku
-post = int(os.environ.get('Port',5000))
+post = int(os.environ.get('Port', 5000))
 
 DB_NAME = "database.db"
 app = Flask(__name__)
 # app.secret_key = "epic haxxxxx"
 app.config['SQLACHEMY_DATABASE_URI'] = 'postgresql:///{DB_NAME}'
-#app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
 db = SQLAlchemy(app)
 
@@ -106,6 +103,59 @@ class Pantry(Resource):
 
 # db.create_all()
 
+fridge_data = [
+    Fridge("Full fridge, freezer, pantry",
+           "29 West Hastings Street",
+           name="Vancouver Women's Health Collective",
+           hrs="11am - 7pm weekdays",
+           description="Community fridge located within the building; folks have to be buzzed in"),
+    Fridge("Full fridge, freezer, pantry, accepts home cooked meals",
+           "29 West Hastings Street",
+           name="Vancouver Women's Health Collective",
+           description="This fridge is located in the alley behind the green house"),
+    Fridge("Full fridge, freezer, pantry",
+           "29 West Hastings Street",
+           name="Vancouver Women's Health Collective",
+           description="This fridge is located in the alley behind the blue house"
+           ),
+    Fridge("Full fridge, freezer, pantry",
+           "340 West 2nd Avenue, Vancouver, BC",
+           name="Food Stash Foundation",
+           description="This fridge is located in the alley behind the blue house"
+           ),
+    Fridge("Full fridge, freezer, pantry",
+           "3718 Main Street, Vancouver, BC",
+           name="The Soap Dispensary & Kitchen Staples",
+           description="Located in the heart of Main Street!",
+           img="https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/215763969_288689355954978_4168483443270479323_n.jpg?"
+               "_nc_cat=103&ccb=1-5&_nc_sid=a26aad&_nc_ohc=AxakfH4qk_oAX9X7fwp&_nc_ht=scontent-sea1-1.xx&oh=00_AT9WT2xN7"
+               "LUFOVRY1e3RePpD1Fq2gX-igzwKqOq97zde0g&oe=61E81965"),
+]
+
+pantry_data = [
+    Pantry(
+        "E 6th street, Vancouver, BC",
+        social_media="@van.periodpantry",
+        description="Located between Commercial & Victoria. E-transfer donations to vancouver.periodpantry@gmail.com",
+        name="Vancouver Period Pantry",
+        website="https://linktr.ee/van.periodpantry"
+    ),
+    Pantry(
+        "1410 W 72nd Ave, Vancouver, BC V6P 3C7",
+        social_media="@freepantrymarpole",
+        description="Take What You Need, Give What You Can",
+        name="Marpole Little Free Pantry",
+        website="https://connect4219.wixsite.com/marpolemutualaid"
+    ),
+    Pantry(
+        "2125 Victoria Dr, Vancouver, BC V5N 5Y4",
+        social_media="@little_free_pantry",
+        description="Take What You Need, Give What You Can",
+        name="McSpadden Little Free Pantry",
+        website="https://www.flyingzucchini.ca/"
+    )
+]
+
 
 @app.route('/')
 def index():
@@ -115,7 +165,7 @@ def index():
 @app.route('/fridges')
 def fridges():
     data = {"fridges": []}
-    for fridge in test_data.fridge_data:
+    for fridge in fridge_data:
         data["fridges"].append(fridge.serialize())
 
     return render_template('fridge.html', api_key=api_key, data=data)
@@ -124,7 +174,7 @@ def fridges():
 @app.route('/pantries')
 def pantries():
     data = {"pantries": []}
-    for pantry in test_data.pantry_data:
+    for pantry in pantry_data:
         data["pantries"].append(pantry.serialize())
 
     return render_template('pantry.html', api_key=api_key, data=data)
