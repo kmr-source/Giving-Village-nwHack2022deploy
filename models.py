230a920ad6e70a5964fce77179818fc960f19faf
kmr-source <kmr.source@gmail.com>
2022-01-15 18:07:35 -0800

    update model.py
    add fridge model + update shelter model

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql import func
 
 
 class Shelter(db.Model):
+    shelterName = db.Column(db.String(1000))
     phoneNumber = db.Column(db.Integer, unique = True)
     location = db.Column(db.String(200), unique = True)
     hours = db.Column(db.Integer)
@@ -12,4 +13,9 @@ class Shelter(db.Model):
 class Pantry(db.Model):
     location = db.Column(db.String(200), unique = True)
     hours = db.Column(db.Integer)
+
+class Fridge(db.Model):
+    location = db.Column(db.String(200), unique =True)
+    hours = db.Column(db.Integer)
+
     
