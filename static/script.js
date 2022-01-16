e41b21bb8d828cd6709b5c115360b17307a1b90d
Jessica Hong <jeessicahong@gmail.com>
2022-01-15 20:49:27 -0800

    multiple markers can be placed

--- static/script.js
+++ static/script.js
@@ -1,14 +1,9 @@
-let geocoder;
-let map;
-
 function initMap() {
     const center = {lat: 40.731, lng: -73.997};
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
+        zoom: 15,
         center: center
     });
-
-    const infoWindow = new google.maps.InfoWindow();
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -16,20 +11,45 @@ function initMap() {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude,
             };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent("Location found.");
-            infoWindow.open(map);
             map.setCenter(pos);
         },
             () => {
                 console.log("Someting went wrong!");
             });
     }
-}
 
-function showLocations() {
+    let marker;
+    let infoWindow;
+    let locations = [
+        {name: "Kensington Pantry", lat: 49.275440467796386, lng: -123.1119283030811, type: "Fridge"},
+        {name: "Women's Shelter", lat: 49.28333290043626, lng: -123.10237964365506, type: "Shelter"}
+    ];
 
+    for (let i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+            map: map,
+            title: locations[i].name
+        });
+
+        (function(marker, i) {
+            google.maps.event.addListener(marker, "click", () => {
+                if (infoWindow) {
+                    infoWindow.close();
+                }
+                infoWindow = new google.maps.InfoWindow({
+                    title: locations[i].name,
+                    content: locations[i].type
+                });
+                infoWindow.open({
+                    map,
+                    anchor: marker,
+                    shouldFocus: true,
+                });
+                map.panTo(marker.getPosition());
+            });
+        })(marker, i);
+    }
 }
 
 initMap();
